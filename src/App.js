bc6df62fe9cc47c101c1399e75504ae534fe6eef
achaldharmale62978 <achaldharmale62978@gmail.com>
2023-10-10 12:58:16 +0530

    teamwork

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Component/Layout/Navbar/Navbar'
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Component/Layout/Home'
 
 function App() {
   return (
     <div className="App">
-     <h3>App.js</h3>
+     <h3>App.js
+      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          {/* layout */}
+          <Route path='home' element={<Home/>}></Route>
+
+          {/* remodule */}
+        <Route></Route>
+
+        </Routes>
+      </BrowserRouter>
+     </h3>
     </div>
   );
 }
